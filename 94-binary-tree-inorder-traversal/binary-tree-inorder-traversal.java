7defe0baff33fc6d824ff8715a7713345c1f132c
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-03-09 11:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (28.95%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // if(root == null) return ans;
+        // inorderTraversal(root.left);
+        // ans.add(root.val);
+        // inorderTraversal(root.right);
+        // return ans;
+
+        //Morris Inorder Traversal
+        TreeNode cur = root;
+
+        while(cur != null){
+
+            if(cur.left == null){
+                ans.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left;
+                while(prev.right!=null && prev.right!=cur){
+                    prev = prev.right;
+                }
+                
+                if(prev.right == null){
+                    prev.right = cur;
+                    cur = cur.left;
+                } else {
+                    prev.right = null;
+                    ans.add(cur.val);
+                    cur = cur.right;
+                }
+
+            }
+        }
+
+        return ans;
+    }
+}
