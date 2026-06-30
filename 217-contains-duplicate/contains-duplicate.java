d76da402f2d33c161d7096801b0e609ea9c827e0
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-06-30 11:47:51 +0530

    Time: 24 ms (23.56%) | Memory: 93.1 MB (70.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length==1) return false;
-        Arrays.sort(nums);
-        for(int i=1; i<nums.length; i++){
-            if(nums[i-1]==nums[i]) return true;
+        
+        Set<Integer> set = new HashSet<>();
+        for(Integer num:nums){
+            set.add(num);
         }
-        return false;
+
+        return set.size()==nums.length? false:true;
     }
 }
