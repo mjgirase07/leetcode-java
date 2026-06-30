139bdc4dded2f73fbc13332b0b6fb55e14c294c6
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-06-30 11:21:27 +0530

    Time: 2 ms (99.60%) | Memory: 44.7 MB (56.85%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] arr = new int[26];
+
+        for(char ch : s.toCharArray()){
+            arr[ch-'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            arr[ch-'a']--;
+            if(arr[ch-'a']<0) return false;
+        }
+        return true;
+    }
+}
