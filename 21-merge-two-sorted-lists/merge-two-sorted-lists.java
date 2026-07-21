80108156d655cf8acab9db410c2f2539cf6d071e
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-07-21 15:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (88.84%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -29,27 +29,19 @@ class Solution {
         while(list1 != null && list2 != null){
             if(list1.val <= list2.val ){
                 temp.next = list1;
-                temp = temp.next;
                 list1 = list1.next;
             } else {
                 temp.next = list2;
-                temp = temp.next;
                 list2 = list2.next;
             }
+            temp = temp.next;
         }
 
-        while(list1 != null){
+        if(list1 != null){
             temp.next = list1;
-            temp = temp.next;
-            list1 = list1.next;
-        }
-
-        while(list2 != null){
+        } else {
             temp.next = list2;
-            temp = temp.next;
-            list2 = list2.next;
         }
-
         return head;
     }
 }
