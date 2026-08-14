c742b9dc23042ad0103ba0e3102a5c5042db61da
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-08-14 13:10:09 +0530

    Time: 1 ms (14.34%) | Memory: 42.2 MB (48.27%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,7 +4,27 @@ class Solution {
         for(int[] arr:dp){
             Arrays.fill(arr,-1);
         }
-        return f(m-1,n-1,dp);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = 1;
+                    continue;
+                } 
+                int up=0,down=0;
+                if(i>0){
+                    up = dp[i-1][j];
+                }
+                if(j>0){    
+                    down = dp[i][j-1]; 
+                }
+                dp[i][j] = up+down;
+            }
+        }
+
+        return dp[m-1][n-1];
+
+        //return f(m-1,n-1,dp);
     }
     int f(int i, int j, int[][] dp){
         if(i==0 || j==0) return 1;
