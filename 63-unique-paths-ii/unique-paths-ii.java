a7cfdf58bf1e3ef0ed55f76da88c64acbb4439f6
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-08-18 10:52:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (65.26%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,35 +1,60 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] og) {
-        int[][] dp = new int[og.length][og[0].length];
+        // int[][] dp = new int[og.length][og[0].length];
         
-        for(int i=0; i<og.length; i++){
-            for(int j=0; j<og[0].length; j++){
-                dp[i][j]=-1;
-            }
-        }
+        // for(int i=0; i<og.length; i++){
+        //     for(int j=0; j<og[0].length; j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
 
-        if(og[0][0] == 1) return 0;
-        
-        for(int i=0; i<og.length; i++){
-            for(int j=0; j<og[0].length; j++){
+        // if(og[0][0] == 1) return 0;
+        int m=og.length;
+        int n=og[0].length;
+
+        int[] prev = new int[n];
+
+        for(int i=0; i<m; i++){
+            int[] temp = new int[n];
+            for(int j=0; j<n; j++){
                 if(og[i][j]==1){
-                    dp[i][j] = 0;
+                     temp[j] = 0;
+                     continue;
+                } 
+                if(i==0 && j==0){
+                    temp[j] = 1;
                     continue;
-                    } 
-                else {
-                    if(i==0 && j==0){
-                        dp[i][j] = 1;
-                        continue;
-                    }
-
-                    int up = (i>0)?dp[i-1][j]:0;
-                    int left = (j>0)?dp[i][j-1]:0;
-
-                    dp[i][j] = up+left;
                 }
+                int up = (i>0)? prev[j]:0;
+                int left = (j>0)? temp[j-1]:0;
+
+                temp[j] = up+left;
             }
+            prev = temp;
         }
-        return dp[og.length-1][og[0].length-1];
+
+        return prev[n-1];
+        
+        // for(int i=0; i<og.length; i++){
+        //     for(int j=0; j<og[0].length; j++){
+        //         if(og[i][j]==1){
+        //             dp[i][j] = 0;
+        //             continue;
+        //             } 
+        //         else {
+        //             if(i==0 && j==0){
+        //                 dp[i][j] = 1;
+        //                 continue;
+        //             }
+
+        //             int up = (i>0)?dp[i-1][j]:0;
+        //             int left = (j>0)?dp[i][j-1]:0;
+
+        //             dp[i][j] = up+left;
+        //         }
+        //     }
+        // }
+        // return dp[og.length-1][og[0].length-1];
         //return func(og.length-1,og[0].length-1,dp,og);
     }
 
