9dc5b2f03991a578ed28ee2e58525914018085ab
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-08-18 12:16:00 +0530

    Time: 6 ms (10.08%) | Memory: 49.9 MB (92.03%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,26 +1,47 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length, n=grid[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] arr: dp){
-            Arrays.fill(arr,-1);
-        }
+
+        int[] prev = new int[n];
 
         for(int i=0; i<m; i++){
+            int[] temp = new int[n];
             for(int j=0; j<n; j++){
                 if(i==0 && j==0){
-                    dp[i][j] = grid[i][j];
+                    temp[j] = grid[i][j];
                     continue;
                 }
 
-                int up = (i>0)?dp[i-1][j]+grid[i][j]:(int)1e9;
-                int down = (j>0)?dp[i][j-1]+grid[i][j]:(int)1e9;
+                int up = (i>0)?prev[j]+grid[i][j]:(int)1e9;
+                int left = (j>0)?temp[j-1]+grid[i][j]:(int)1e9;
 
-                dp[i][j] = Math.min(up,down);
+                temp[j] = Math.min(up,left);
             }
+            prev = temp;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
+
+        // int[][] dp = new int[m][n];
+        // for(int[] arr: dp){
+        //     Arrays.fill(arr,-1);
+        // }
+
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(i==0 && j==0){
+        //             dp[i][j] = grid[i][j];
+        //             continue;
+        //         }
+
+        //         int up = (i>0)?dp[i-1][j]+grid[i][j]:(int)1e9;
+        //         int down = (j>0)?dp[i][j-1]+grid[i][j]:(int)1e9;
+
+        //         dp[i][j] = Math.min(up,down);
+        //     }
+        // }
+
+        // return dp[m-1][n-1];
         //return func(m-1,n-1,dp,grid);
         
     }
