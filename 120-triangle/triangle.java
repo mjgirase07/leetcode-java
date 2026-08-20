0b10ae70cb2fe3a7f687743743063613168df36f
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-08-20 15:02:24 +0530

    Time: 5 ms (33.55%) | Memory: 45.3 MB (75.50%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,26 +1,45 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[triangle.size()][triangle.size()];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
-        }
 
-        for(int i=0; i<triangle.get(n-1).size(); i++){
-            dp[n-1][i] = triangle.get(n-1).get(i);
+        int[] front = new int[n];
+
+        for(int j=0; j<triangle.get(n-1).size(); j++){
+            front[j] = triangle.get(n-1).get(j);
         }
 
         for(int i=n-2; i>=0; i--){
+            int[] cur = new int[n];
             for(int j=i; j>=0; j--){
-                int down = triangle.get(i).get(j)+dp[i+1][j];
-                int diag = triangle.get(i).get(j)+dp[i+1][j+1];
-
-                dp[i][j] = Math.min(down,diag);
-                
+                int down = triangle.get(i).get(j)+front[j];
+                int diag = triangle.get(i).get(j)+front[j+1];
+                cur[j] = Math.min(down,diag);
             }
+            front = cur;
         }
+        
+        return front[0];
 
-        return dp[0][0];
+        // int[][] dp = new int[triangle.size()][triangle.size()];
+        // for(int[] arr:dp){
+        //     Arrays.fill(arr,-1);
+        // }
+
+        // for(int i=0; i<triangle.get(n-1).size(); i++){
+        //     dp[n-1][i] = triangle.get(n-1).get(i);
+        // }
+
+        // for(int i=n-2; i>=0; i--){
+        //     for(int j=i; j>=0; j--){
+        //         int down = triangle.get(i).get(j)+dp[i+1][j];
+        //         int diag = triangle.get(i).get(j)+dp[i+1][j+1];
+
+        //         dp[i][j] = Math.min(down,diag);
+                
+        //     }
+        // }
+
+        // return dp[0][0];
         //return func(0,0,triangle,dp);
     }
     int func(int i, int j, List<List<Integer>> t, int[][] dp){
