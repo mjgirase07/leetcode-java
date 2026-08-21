e732a5554d48a6d55727cae495c56df4fc366c6a
Mahendra Girase <110486261+mjgirase07@users.noreply.github.com>
2026-08-21 12:32:12 +0530

    Time: 38 ms (14.27%) | Memory: 46.1 MB (94.12%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -3,13 +3,47 @@ class Solution {
         int r=grid.length,c=grid[0].length;
         int[][][] dp = new int[r][c][c];
 
-        for(int[][] arr:dp){
-            for(int[] arr2:arr){
-                Arrays.fill(arr2,-1);
+        for(int j1=0; j1<c; j1++){
+            for(int j2=0;j2<c; j2++){
+                if(j1==j2) dp[r-1][j1][j2] = grid[r-1][j1];
+                else dp[r-1][j1][j2] = grid[r-1][j1]+grid[r-1][j2];
             }
         }
 
-        return func(0,0,grid[0].length-1,grid.length,grid[0].length,grid,dp);
+
+        for(int i=r-2; i>=0; i--){
+            for(int j1=0; j1<c; j1++){
+                for(int j2=0; j2<c; j2++){
+
+                    int maxi = -(int)1e8;
+                    
+                    for(int dj1 = -1; dj1<=1; dj1++){
+                        for(int dj2 = -1; dj2<=1; dj2++){
+                            
+                            int value = 0;
+                            if(j1==j2) value = grid[i][j1];
+                            else value = grid[i][j1]+grid[i][j2];
+
+                            if(j1+dj1>=0 && j1+dj1<c && j2+dj2>=0 && j2+dj2<c)
+                                value+=dp[i+1][j1+dj1][j2+dj2];
+                            else value = -(int) 1e8;
+                            maxi = Math.max(maxi,value);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][c-1];
+
+        // for(int[][] arr:dp){
+        //     for(int[] arr2:arr){
+        //         Arrays.fill(arr2,-1);
+        //     }
+        // }
+
+        // return func(0,0,grid[0].length-1,grid.length,grid[0].length,grid,dp);
     }
 
     int func(int i, int j1, int j2, int r, int c, int[][] grid, int[][][] dp){
